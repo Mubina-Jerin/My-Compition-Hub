3e36d52f712f73a0d71b33f7c7498f9ebf9bf4f4
Mubina-Jerin <mubinarahamanjerin@gmail.com>
2019-04-13 02:01:02 +0600

    added model class

--- app/src/main/java/com/jerin/competetionhub/MainActivity.java
+++ app/src/main/java/com/jerin/competetionhub/MainActivity.java
@@ -9,8 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView myTextView;
-    Button myButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,23 +17,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        myTextView = findViewById(R.id.myTextView);
-        myButton = findViewById(R.id.myButton);
-
-
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // so
-
-
-                Abstract jerin = new Abstract("Jerimn", "151", "Bonosri", "017");
-                Abstract juboraj = new Abstract("Juboraj", "151-15-270", "Uttara", "019");
-
-                Toast.makeText(MainActivity.this, juboraj.getId(), Toast.LENGTH_SHORT).show();
-
-            }
-        });
     }
-}
+    }
--- app/src/main/java/com/jerin/competetionhub/MyInheritence.java
+++
@@ -1,7 +0,0 @@
-package com.jerin.competetionhub;
-
-public interface MyInheritence {
-
-     void transfer();
-     void print();
-}
