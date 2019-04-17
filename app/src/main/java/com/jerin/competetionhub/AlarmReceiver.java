4775ba85ef830e6ab43145830117c91bb450be36
Mubina-Jerin <mubinarahamanjerin@gmail.com>
2019-04-17 23:42:56 +0600

    added Notification activity,competition activity,Request handler,Volley

---
+++ app/src/main/java/com/jerin/competetionhub/AlarmReceiver.java
@@ -0,0 +1,55 @@
+package com.jerin.competetionhub;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import static android.app.NotificationManager.IMPORTANCE_DEFAULT;
+
+public class AlarmReceiver extends BroadcastReceiver{
+
+    private static final String CHANNEL_ID = "com.cse.android.channelId";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = new Intent(context, NotificationActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(NotificationActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(context);
+
+        // notification bar view
+        Notification notification = builder.setContentTitle("Registration Deadline Approaching!")
+                .setContentText("Hurry Up!.. Click here to participate!")
+                .setTicker("New Message Alert!")
+                .setSmallIcon(R.mipmap.logo1)
+                .setContentIntent(pendingIntent).build();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setChannelId(CHANNEL_ID);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "NotificationDemo",
+                    IMPORTANCE_DEFAULT
+            );
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0, notification);
+    }
+}
